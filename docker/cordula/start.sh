50724386f64cc294a5555895f07548bfdcb13198
Matthieu Sprunck <matthieu@vigiglobe.com>
2016-11-25 17:02:08 +0100

    Fix docker image. Use env vars instead of app args.

--- docker/cordula/start.sh
+++ docker/cordula/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /usr/bin/java -Dlog4j.debug -Dlog4j.configuration=file:/log4j.properties \
-              -jar /cordula.jar --host 0.0.0.0 --port 3000 "$@"
+              -jar /cordula.jar "$@"
